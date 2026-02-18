95c4997471a011a06d26794b709635b6df6f49ed
Duha <s445005974@uqu.edu.sa>
2026-02-18 21:59:16 +0300

    i add Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
-import Sidebar from "./components/Sidebar"
-import Header from "./components/Header"
+
 import Dashboard from "./components/Dashboard"
-
-import { useState } from "react"; 
+import Layout from "./components/Layout"
 function App() {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
 
-    function toggleSidebar() {
-if (isSidebarOpen === true) {
-setIsSidebarOpen(false);
-} else {
-setIsSidebarOpen(true);
-}}
+
   return (
-    <div className=" grid grid-cols-[auto_1fr]">
-<Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-<div className="flex flex-col"> 
-     <Header isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
-     <Dashboard/>
-  </div>
+    <div className="">
+
+<Layout >
+  <Dashboard/>
+  </Layout>
+
 </div>  
 )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,33 @@
 import Header from './Header'
 import Sidebar from './Sidebar';
+import { useState } from "react"; 
+
 export default function Layout({children}){
+{/** Business logic */}
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+function toggleSidebar() {
+if (isSidebarOpen === true) {
+setIsSidebarOpen(false);
+} else {
+setIsSidebarOpen(true);
+}}
+
+
+{/** UI logic */}
 
 return(
-<div>
+<div className=" grid grid-cols-[auto_1fr]">
+<Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
 
-
-<Header/>
-<Sidebar/>
+<div className="flex flex-col"> 
+<Header isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
 {children}
+
+  </div>
+  
+
+
 
 </div>
 );
